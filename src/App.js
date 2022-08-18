6288936cf69878333545cbcd72613c7d93009e3a
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-08-18 18:08:29 +0530

    Added routes and components

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import CertificateGenerator from "./components/CertificateGenerator";
+import EmailSender from "./components/EmailSender";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+import OfferLetter from "./components/OfferLetter";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/cerificate-generator"
+            element={<CertificateGenerator />}
+          />
+          <Route path="/email-sender" element={<EmailSender />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/offer-letter" element={<OfferLetter />} />
+          <Route
+            path="*"
+            element={
+              <section style={{ padding: "1rem" }}>
+                <p>THere's notHing Here!</p>
+              </section>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EmailSender.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EmailSender() {
+  return <div>EmailSender</div>;
+}
+
+export default EmailSender;
